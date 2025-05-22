3f6ed4371a0a682fa1086451738cd47ad837d446
Muhammad Syahrul Gunawan <syahrulgunawann41@gmail.com>
2025-05-22 15:27:32 +0700

    Asset Project

---
+++ resources/views/admin/kelolaDataLomba/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.template')
+
+@section('content')
+            <div class="layout-px-spacing">
+
+                <div class="page-header">
+                    <div class="page-title">
+                        <h3>Blank Page</h3>
+                    </div>
+                </div>
+
+
+                <!-- CONTENT AREA -->
+
+
+
+
+
+                <!-- CONTENT AREA -->
+
+            </div>
+@endsection
