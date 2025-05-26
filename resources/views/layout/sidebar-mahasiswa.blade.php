9cfb70146601e78580f5417f85a07755635d1c17
DewitaA12 <xsclthzn@gmail.com>
2025-05-26 12:03:26 +0700

    add  sidebar untuk beranda, profil, prestasi, dan lomba

---
+++ resources/views/layout/sidebar-mahasiswa.blade.php
@@ -0,0 +1,54 @@
+        <div class="sidebar-wrapper sidebar-theme">
+
+            <div id="dismiss" class="d-lg-none"><i class="flaticon-cancel-12"></i></div>
+
+            <nav id="sidebar">
+
+                <ul class="navbar-nav theme-brand flex-row  text-center">
+                    <li class="nav-item theme-text">
+                        <a href="index.html" class="nav-link"> COMPQUEST </a>
+                    </li>
+                </ul>
+
+                <ul class="list-unstyled menu-categories" id="accordionExample">
+                    <li class="menu">
+                        <a href="{{ route('mahasiswa.beranda') }}" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                <span> Beranda </span>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li class="menu">
+                        <a href="{{ route('mahasiswa.profil') }}" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-3-3.87"></path><path d="M4 21v-2a4 4 0 0 1 3-3.87"></path><circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                                <span> Profil </span>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li class="menu">
+                    <a href="{{ route('mahasiswa.prestasi') }}" aria-expanded="false" class="dropdown-toggle">
+                        <div class=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-award"> <circle cx="12" cy="8" r="7"></circle><polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline></svg>
+                        <span> Prestasi </span>
+                        </div>
+                    </a>
+                    </li>
+
+
+                    <li class="menu">
+                        <a href="{{ route('mahasiswa.lomba') }}" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/></svg>
+                                <span> Lomba </span>
+                            </div>
+                        </a>
+                    </li>
+   
+                </ul>
+            </nav>
+
+        </div>
