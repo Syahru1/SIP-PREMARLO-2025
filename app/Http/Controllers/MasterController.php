8acce57f42bd125ec5877213424e1ff2b11ba968
Muhammad Syahrul Gunawan <syahrulgunawann41@gmail.com>
2025-06-03 18:18:46 +0700

    Controller

---
+++ app/Http/Controllers/MasterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MasterController extends Controller
+{
+    public function dosen()
+    {
+        return view('dosen.beranda.index',[
+            "title" => "Dashboard"
+        ]);
+        // if (auth()->user()->role == 'Owner') {
+        // }
+
+        return redirect()->back();
+    }
+
+    public function admin()
+    {
+        return view('admin.beranda',[
+            "title" => "Dashboard"
+        ]);
+        // if (auth()->user()->role == 'Admin') {
+        // }
+
+        return redirect()->back();
+    }
+
+    public function mahasiswa()
+    {
+        return view('mahasiswa.beranda.index',[
+            "title" => "Dashboard"
+        ]);
+        // if (auth()->user()->role == 'Kasir') {
+        // }
+
+        return redirect()->back();
+    }
+
+    public function index()
+    {
+        return view('general.landing');
+    }
+}
